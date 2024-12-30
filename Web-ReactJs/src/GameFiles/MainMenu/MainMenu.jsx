324a82c6e3cab9e03b4fd0d1ded72e9e32a373d2
diana <99146180+aiexsilva@users.noreply.github.com>
2024-12-30 17:43:11 +0000

    main screen
    check

--- Web-ReactJs/src/GameFiles/MainMenu/MainMenu.jsx
+++ Web-ReactJs/src/GameFiles/MainMenu/MainMenu.jsx
@@ -2,7 +2,9 @@ import React, { useState } from 'react';
 import Login from '../Login/Login';
 import '../../index.css';
 
+
 function MainMenu() {
+
   const [showLogin, setShowLogin] = useState(false);
 
   if (showLogin) {
@@ -10,15 +12,19 @@ function MainMenu() {
   }
 
   return (
-    <div>
-      <h1 className='gameTitle'>[XXXXX]</h1>  
+    <body>
+      <div class = "retro-container">
+        <div class = "bg-terminal">
+          <h1 className='gameTitle'>77 73 83 83 73 79 78 32 88 88 73 10</h1>  
 
-      <div className='menuButtons'>
-        <button className='startButton'>Start Game</button>
-        <button className='achivButton'>Achievements</button>
-        <button className='loginButton' onClick={() => setShowLogin(true)}>Log In</button>
+          <div className='menuButtons'>
+            <button className='startButton'>Start Game</button>
+            <button className='achivButton'>Achievements</button>
+            <button className='loginButton' onClick={() => setShowLogin(true)}>Log In</button>
+          </div>
+        </div>
       </div>
-    </div>
+    </body>
   );
 }
 
