469d8022d3d7855b62ac9c28da2ceb122ca817f4
Layfyy <150027558+15LucasAlves@users.noreply.github.com>
2024-12-30 01:41:24 +0000

    .

--- Web-ReactJs/src/main.jsx
+++ Web-ReactJs/src/main.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
 import './index.css'
 import './GameFiles/GameStateManager.jsx'
+import StateManager from './GameFiles/GameStateManager.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <>
   
     <h1>Game Title</h1>
     <button type="button">Start Game</button>
     <StateManager />
 
-  </StrictMode>
+  </>
 )
