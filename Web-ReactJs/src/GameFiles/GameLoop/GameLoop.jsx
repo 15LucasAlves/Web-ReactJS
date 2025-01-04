133ef604d7b94341b74ac5818bb56132cb188db9
Layfyy <150027558+15LucasAlves@users.noreply.github.com>
2025-01-04 18:07:51 +0100

    :(

--- Web-ReactJs/src/GameFiles/GameLoop/GameLoop.jsx
+++ Web-ReactJs/src/GameFiles/GameLoop/GameLoop.jsx
@@ -1,11 +1,12 @@
 import 'react';
 import '../../index.css';
+import StateManager from '../GameStateManager';
 
 function GameLoop() {
     return (
         <div>
             <h1>Game Loop</h1>
-            <button>Back to Main Menu</button>
+            <button onClick={ () => StateManager("MainMenu")}>Back to Main Menu</button>
         </div>
     );
 }
