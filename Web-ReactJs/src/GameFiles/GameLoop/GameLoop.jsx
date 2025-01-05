eb31c0525e34e9d93fbde005533ff7b8468d6fdb
diana <99146180+aiexsilva@users.noreply.github.com>
2025-01-05 21:17:00 +0000

    working css:)

--- Web-ReactJs/src/GameFiles/GameLoop/GameLoop.jsx
+++ Web-ReactJs/src/GameFiles/GameLoop/GameLoop.jsx
@@ -10,15 +10,19 @@ function GameLoop() {
     //sets constant to behave like tab instead of button, terminal is the default one
     const [activeTab, setActiveTab] = useState("Terminal");
 
-    const dialogs = dialogsData.dialogs; // Lista de diálogos
-  const [currentDialog, setCurrentDialog] = useState(dialogs[0]); // Primeiro diálogo
-  const [mensagem, setMensagem] = useState(""); // Texto acumulado
-  const [typingText, setTypingText] = useState(""); // Texto sendo digitado
-  const [showDecisions, setShowDecisions] = useState(false); // Exibe as decisões
-  const [decisions, setDecisions] = useState([]); // Lista de decisões disponíveis
-  const [inputValue, setInputValue] = useState(""); // Valor do input do usuário
+    /*const toggleTab = (tab) => {
+        setActiveTab(tab);
+    };*/
 
-  const typingSpeed = 1; // Velocidade de typing em milissegundos
+    const dialogs = dialogsData.dialogs; // Lista de diálogos
+    const [currentDialog, setCurrentDialog] = useState(dialogs[0]); // Primeiro diálogo
+    const [mensagem, setMensagem] = useState(""); // Texto acumulado
+    const [typingText, setTypingText] = useState(""); // Texto sendo digitado
+    const [showDecisions, setShowDecisions] = useState(false); // Exibe as decisões
+    const [decisions, setDecisions] = useState([]); // Lista de decisões disponíveis
+    const [inputValue, setInputValue] = useState(""); // Valor do input do usuário
+
+    const typingSpeed = 1; // Velocidade de typing em milissegundos
   
   useEffect(() => {
     if (!currentDialog) return;
@@ -78,7 +82,7 @@ function GameLoop() {
         setDecisions([]); // Limpa as decisões
         setInputValue(""); // Reseta o valor do input
       } else {
-        alert("Escolha inválida. Digite um número válido.");
+        alert("Invalid choice. Type a valid number.");
         setInputValue(""); // Reseta o valor do input
       }
     }
@@ -92,12 +96,18 @@ function GameLoop() {
                 <div
       style={{
         padding: "10px",
-        fontFamily: "Arial, sans-serif",
+        fontFamily: "Share Tech Mono, monospace",
         lineHeight: "1.5",
-        position: "relative",
-        minHeight: "100vh",
+        position: "absolute",
+        top: 60,
+        left: 10,
+        maxHeight: "72vh",
         boxSizing: "border-box",
-        color: "#fff", // Define o texto como branco
+        overflowY: "auto",
+        color: "rgb(var(--fg-pb-green))", 
+        display: "flex",  
+        flexDirection: "column", 
+        justifyContent: "flex-end",
       }}
     >
       {/* Texto acumulado com <br /> já tratado no JSON */}
@@ -122,7 +132,7 @@ function GameLoop() {
       {/* Mostra as decisões logo abaixo do último diálogo */}
       {showDecisions && (
         <div>
-          <p>Escolha uma das opções abaixo:</p>
+          <p>Choose one of the options below:</p>
           <ul style={{ marginLeft: "20px" }}>
             {decisions.map((decision) => (
               <li key={decision.id}>
@@ -161,29 +171,28 @@ function GameLoop() {
                         
                         <div className='inputcontain'>
                             {/* Caixa de input para escolhas */}
-      {showDecisions && (
+                            {showDecisions && (
        
-          <input placeholder=">....." className='input'
-            type="text"
-            value={inputValue}
-            onChange={(e) => setInputValue(e.target.value)}
-            onKeyDown={handleDecisionInput}
-            style={{
-              width: "100%",
-              padding: "10px",
-              borderRadius: "5px",
-              border: "1px solid #444",
-              fontSize: "16px",
-              backgroundColor: "#222", // Fundo do input escuro
-              color: "#fff", // Texto branco no input
-            }}
-          />
+                            <input placeholder=">....." className='input'
+                                    type="text"
+                                     value={inputValue}
+                                     onChange={(e) => setInputValue(e.target.value)}
+                                    onKeyDown={handleDecisionInput}
+                            style={{
+                            width: "99%",
+                            padding: "10px",
+                            borderRadius: "5px",
+                            border: "1px solid #444",
+                            fontSize: "16px",
+                            backgroundColor: "#222", // Fundo do input escuro
+                            color: "rgb(var(--fg-pb-green))", 
+                        }}
+                     />
         
-      )}
+                )}
                             
                         </div>
                     )}
-
                     {activeTab === "MissionLog" && (
                         <div className='missionLog'>
                         </div>
